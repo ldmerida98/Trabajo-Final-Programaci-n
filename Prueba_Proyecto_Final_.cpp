8af4e567961682f697d9412267231b02f1874660
ldmerida98 <93799564+ldmerida98@users.noreply.github.com>
2021-11-18 23:21:31 -0600

    Update Prueba_Proyecto_Final_.cpp

--- Prueba_Proyecto_Final_.cpp
+++ Prueba_Proyecto_Final_.cpp
@@ -1,1 +1,16 @@
+#include <stdio.h>	
+#include <time.h>
+#include <stdlib.h>
 
+int main() {
+	system("color 40");
+	int anio;
+	int opcion=1, dia=1, mes=1;
+  
+  time_t fecha; //
+	struct tm *ahora;
+	time(&fecha); 
+	ahora=localtime(&fecha); 
+	dia=ahora->tm_mday;
+	mes=ahora->tm_mon+1; 
+	anio = (ahora->tm_year)+1900;
