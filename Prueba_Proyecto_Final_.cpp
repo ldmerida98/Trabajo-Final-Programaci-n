d1d115e2e37a6f71af1cb8cee39c48f2eb266a77
IVillegas22 <93788354+IVillegas22@users.noreply.github.com>
2021-11-18 23:22:16 -0600

    Update Prueba_Proyecto_Final_.cpp

--- Prueba_Proyecto_Final_.cpp
+++ Prueba_Proyecto_Final_.cpp
@@ -14,3 +14,6 @@ int main() {
 	dia=ahora->tm_mday;
 	mes=ahora->tm_mon+1; 
 	anio = (ahora->tm_year)+1900;
+        system ("cls"); 
+	printf("\t\t\t--------------------CALENDARIO--------------------\n\n"); 
+	printf(" > La fecha de hoy es: %d/%d/%d \n\n\n",dia,mes,anio); 
